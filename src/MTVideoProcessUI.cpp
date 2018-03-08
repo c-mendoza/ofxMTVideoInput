8a7a899bde47f1e9c74ea5e308611e97f35b9a37
c-mendoza <c@matadata.com>
2018-03-07 20:37:27 -0500

    Added MTVideoProcessUIWithImage

--- src/MTVideoProcessUI.cpp
+++ src/MTVideoProcessUI.cpp
@@ -5,12 +5,44 @@
 #include "MTVideoProcessUI.hpp"
 #include "MTVideoProcess.hpp"
 #include "ofThreadChannel.h"
+#include "MTVideoProcessStream.hpp"
 
 MTVideoProcessUI::MTVideoProcessUI(std::shared_ptr<MTVideoProcess> videoProcess)
 {
 	this->videoProcess = videoProcess;
 }
+
 void MTVideoProcessUI::draw(ofxImGui::Settings& settings)
 {
 	ofxImGui::AddGroup(videoProcess.lock()->getParameters(), settings);
+}
+
+MTVideoProcessUIWithImage::
+MTVideoProcessUIWithImage(std::shared_ptr<MTVideoProcess> videoProcess,
+						  ofImageType imageType) : MTVideoProcessUI(videoProcess)
+{
+	auto stream = videoProcess->processChain.lock();
+	outputImage.allocate(stream->processWidth, stream->processHeight, imageType);
+	addEventListener(videoProcess->processCompleteFastEvent.newListener([this](
+			const MTVideoProcessFastEventArgs<MTVideoProcess>& args)
+																		{
+																			ofPixels pixels;
+																			ofxCv::toOf(args.processOutput, pixels);
+																			outputChannel.send(std::move(pixels));
+																		}));
+}
+
+MTVideoProcessUIWithImage::~MTVideoProcessUIWithImage()
+{
+	outputChannel.close();
+}
+
+void MTVideoProcessUIWithImage::draw(ofxImGui::Settings& settings)
+{
+	ofPixels pixels;
+	while (outputChannel.tryReceive(pixels))
+	{
+		outputImage.setFromPixels(pixels);
+	}
+	ofxImGui::AddImage(outputImage, glm::vec2(outputImageWidth, outputImageHeight));
 }
--- src/MTVideoProcessUI.hpp
+++ src/MTVideoProcessUI.hpp
@@ -5,6 +5,7 @@
 #ifndef NERVOUSSTRUCTUREOF_MTVIDEOPROCESSVIEW_HPP
 #define NERVOUSSTRUCTUREOF_MTVIDEOPROCESSVIEW_HPP
 
+#include <utils/ofThreadChannel.h>
 #include "ofxImGui.h"
 #include "MTAppFrameworkUtils.hpp"
 
@@ -23,6 +24,26 @@ public:
 
 protected:
 	std::weak_ptr<MTVideoProcess> videoProcess;
+};
+
+class MTVideoProcessUIWithImage : public MTVideoProcessUI
+{
+public:
+	MTVideoProcessUIWithImage(std::shared_ptr<MTVideoProcess> videoProcess, ofImageType imageType);
+	~MTVideoProcessUIWithImage();
+
+	/**
+	 * @brief Draws the video process output as an image via
+	 * ofxImGui::AddImage.
+	 * @param settings
+	 */
+	void draw(ofxImGui::Settings& settings) override;
+	int outputImageWidth = 320;
+	int outputImageHeight = 240;
+
+protected:
+	ofImage outputImage;
+	ofThreadChannel<ofPixels> outputChannel;
 };
 
 #endif //NERVOUSSTRUCTUREOF_MTVIDEOPROCESSVIEW_HPP
